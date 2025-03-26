dfd9acbad2f30ba96eb531aac62334e4c211e4e5
AndrePacolla <andreluizzpacolla@outlook.com>
2025-03-26 18:52:53 -0300

    initial componente Hero

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -1,7 +1,34 @@
 export const Hero = () => {
     return(
-        <main>
-            <h1>Hero Componente</h1>
-        </main>
+        <section className="bg-[#e84c3d] text-white relative overflow-hidden">
+            <div>
+                <article>
+                  <div>
+                    <h1 className=" text-3xl md:text-4xl lg:text-5xl font-bold ">Seu pet merece cuidado, carinho e atenção especial.</h1>
+                    <p>
+                      Oferecemos os melhores serviços para garantir o bem-estar e a
+                      felicidade do seu amigo de quatro patas.
+                    </p>
+                    <div>
+                        <a href="#">
+                            Contato via WhatsApp
+                        </a>    
+                    </div>
+                    <div className="mt-8">
+                        <p className="text-sm mb-4">
+                            <b className="bg-black text-white px-2 py-1 rounded-lg">5%</b>de desconto na primeira compra.
+                        </p>
+                    </div>
+                        
+                  </div>
+
+                  <div>
+
+                  </div>
+                </article>
+
+            </div>
+           
+        </section>
     )
 };
