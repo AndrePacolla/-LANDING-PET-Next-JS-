cf1328563dab32f0f46cf177dfcd6c7f3d8bfe4d
AndrePacolla <andreluizzpacolla@outlook.com>
2025-03-27 21:40:55 -0300

    install phosphor

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -1,19 +1,23 @@
+import {WhatsappLogo} from "@phosphor-icons/react/dist/ssr";
 export const Hero = () => {
     return(
         <section className="bg-[#e84c3d] text-white relative overflow-hidden">
             <div>
                 <article>
-                  <div>
-                    <h1 className=" text-3xl md:text-4xl lg:text-5xl font-bold ">Seu pet merece cuidado, carinho e atenção especial.</h1>
-                    <p>
+                  <div className="leading-14">
+                    <h1 className=" text-3xl  md:text-4xl lg:text-5xl font-bold py-5">
+                        Seu pet merece cuidado, carinho e atenção especial.</h1>
+                    <p className="lg:text-lg">
                       Oferecemos os melhores serviços para garantir o bem-estar e a
                       felicidade do seu amigo de quatro patas.
                     </p>
-                    <div>
-                        <a href="#">
+                        <a href="#"
+                        className="bg-green-500 px-7 rounded-md font-semibold flex items-center justify-between w-fit"
+                        >
+                            <WhatsappLogo/>
                             Contato via WhatsApp
                         </a>    
-                    </div>
+              
                     <div className="mt-8">
                         <p className="text-sm mb-4">
                             <b className="bg-black text-white px-2 py-1 rounded-lg">5%</b>de desconto na primeira compra.
