9e241ed02f9a31e04207266b03251fdcd4e6877c
AndrePacolla <andreluizzpacolla@outlook.com>
2025-04-25 16:25:58 -0300

    create function prev e next carousel

--- src/app/_components/services.tsx
+++ src/app/_components/services.tsx
@@ -53,16 +53,25 @@ export const Services = () => {
     }
   })
 
+  const scrollPrev = () => {
+    emplaApi?.scrollPrev();
+  };
+
+  const scrollNext = () => {
+    emplaApi?.scrollNext()
+  };
+
     return(
         <section className="text-black py-16">
             <div className="container mx-auto px-4">
-                <h2 className="text-4xl text-center font-bold md:text-5xl mb-12 ">Nossos Serviços</h2>
+                <h2 className="text-4xl font-bold md:text-5xl mb-12 ">Nossos Serviços</h2>
             <div className="relative">
               <div className="overflow-hidden" ref={emblaRef}>
+
                 <div className="flex">
                   {services.map((item, index) => (
                     <div key={index} className="  flex-[0_0_100%] min-w-0 md:flex-[0_0_calc(100%/3)] px-3"> 
-                      <article className="bg-[#1e293b] text-white rounded-2xl p-6  h-full flex flex-col">
+                      <article className="bg-[#1e293b] text-white rounded-2xl p-6 space-y-3 h-full flex flex-col">
                          <div className="flex items-start justify-between">
                           <div className="flex gap-3 mb-5">
                             <span className="text-3xl">{item.icon}</span>
@@ -72,33 +81,44 @@ export const Services = () => {
                             </div>
                          </div>
                         </div>
-                        <div className="border-t border-gray-700 flex items-center justify-between">
-                          <div className="flex mt-4 gap-3">
+                        <div className="border-t border-gray-700 flex items-center justify-between my-2 pt-4">
+                          <div className="flex gap-3">
                             <Clock className="w-5 h-5"/>
                             <span>{item.duration}</span>
                           </div>
                          
-                          <a className="flex items-center justify-center  pt-4 gap-2"
+                          <a className="flex items-center justify-center gap-2 rounded-lg p-2 hover:bg-red-500 hover:text-black  duration-300"
                           href="#">
-                            <WhatsappLogo className="w-5 h-5"/>
-                           <span>Entrar em contato</span> 
-
+                           <WhatsappLogo className="w-5 h-5"/>
+                           <span className=" hover:text-black">Entrar em contato</span> 
                           </a>   
 
                         </div>
-
                       </article>
-
                     </div>
-
                   ))}
-
-
                 </div>
               </div>
 
-            </div>
+              <button 
+                className="w-10 h-10  bg-[#e84c3d] flex items-center justify-center rounded-full shadow-lg absolute left-3 -translate-y-1/2 -translate-x-1/2 
+                top-1/2 z-10"
+                onClick={scrollPrev}
+                >
+                <ChevronLeft className="w-6 h-6 text-white"
+               />
+              </button>
 
+              <button 
+                className="w-10 h-10  bg-[#e84c3d] flex items-center justify-center rounded-full shadow-lg absolute -right-6 -translate-y-1/2 -translate-x-1/2 
+                top-1/2 z-10"
+                onClick={scrollNext}
+                >
+                <ChevronRight className="w-6 h-6 text-white"
+               />
+              </button>
+
+            </div>
             </div>
         </section>
     )
