5482a56361b39fa858dd0ce4fe14cd826db44833
AndrePacolla <andreluizzpacolla@outlook.com>
2025-05-05 16:17:48 -0300

    array brands

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,7 +1,29 @@
+import golden from "../../../public/golden.png";
+import royal from "../../../public/royal.png";
+import premier from "../../../public/primier.png";
+import whiskas from "../../../public/whiskas.png";
+import natural from "../../../public/natural.png";
+
+
+
+
+
+
+const brands = [
+    { name: "Royal Canil", logo: royal},
+    { name: "Golden", logo: golden},
+    { name: "Premier", logo: premier},
+    { name: "Formula Natural", logo: natural},
+    { name: "Whiskas", logo: whiskas}
+]
+
+
 export const Footer = () => {
+
+
     return(
         <section>
-          
+          ..
         </section>
     )
 }
