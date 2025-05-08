8d852090dfd1a01af8f3ee61d66d5031ccf94130
AndrePacolla <andreluizzpacolla@outlook.com>
2025-05-08 15:52:59 -0300

    photos marcas

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -3,7 +3,7 @@ import royal from "../../../public/royal.png";
 import premier from "../../../public/primier.png";
 import whiskas from "../../../public/whiskas.png";
 import natural from "../../../public/natural.png";
-
+import Image from "next/image";
 
 
 
@@ -14,6 +14,7 @@ const brands = [
     { name: "Golden", logo: golden},
     { name: "Premier", logo: premier},
     { name: "Formula Natural", logo: natural},
+    { name: "Golden", logo: golden},
     { name: "Whiskas", logo: whiskas}
 ]
 
@@ -22,8 +23,33 @@ export const Footer = () => {
 
 
     return(
-        <section>
-          ..
+        <section className="bg-[#E84c3d] py-16 text-white">
+          <div className="container mx-auto px-4">
+            <div className="border-b border-white/20  pb-8">
+                <h4 className="text-4xl font-bold mb-14 text-center">Marcas que trabalhamos</h4>
+                <div className="grid grid-cols-2 lg:grid-cols-6 gap-8">
+                {brands.map((item, index) => (
+                    <div key={index} className="bg-white p-4 rounded-lg flex items-center justify-center">
+                        <Image
+                        src={item.logo}
+                        alt={item.name}
+                        width={150}
+                        height={50}
+                        quality={100}
+                        style={{
+                            width:"auto",
+                            height:"auto"
+                        }}
+
+                        className="object-contain"                        
+                        />
+                    </div>
+                ))}
+            </div>
+            </div>
+     
+
+          </div>
         </section>
     )
 }
